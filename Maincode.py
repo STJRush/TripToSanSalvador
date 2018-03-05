f7b6d16eba6e6932a2c344583102aa7e3d5af90e
MarkMahoneyRush <16MMahoney@stjosephsrush.ie>
2018-03-05 15:22:38 +0000

    Maincode
    Here it is!

---
+++ Maincode.py
@@ -0,0 +1,192 @@
+from time import sleep #this will slow down the code
+
+while True: #this will repeat the code #this is also the start of Marks code
+    print("Welcome to our Educational App!") #this writes out the code
+    sleep(2) #this slows the code by 2 seconds
+    print("There will be games to help you learn")
+    sleep(2)
+    choice=input("You go to college and choose to be a explorer in the age of exploration, which will contain columbus facts. Just type in explorer and we will get started!")
+    break #this breaks out of the code
+while True:
+     if choice == "explorer": #this will let you choose the explorer
+        print("Good choice! This will help you learn about Columbus and the times of exploration,  while your being chased by angry vikings!")
+        break
+   
+print("Welcome to the game. This will help you educate about everything. You will be chased by Aliens.")
+
+while True:
+    if choice == "explorer":
+        print("Its 1487, you have just started your voyage of exploration for America, do you know Colombus set sail for America on 12th August, 1492?")
+        sleep(5)
+        print("Columbus was born in Genoa, Italy in 1451, where you are also born")
+        sleep(5)
+        print("Back in these times, many sailors tried to persuade kings and queens to give them money and ships! You managed to persuade the king of England, Edward the 6th!")
+        sleep(5)
+        print("you look at the hull, the new caravel ship looks steady as she sails onwards!")    
+        sleep(5)
+        print("You look at your logbook, you see that you have only travelled 1,000 kilometres, and the crew are worried")
+        sleep(5)
+        print("Back in these times, Columbus kept 2 logbooks, one with false infromation to calm the crew, and one with the correct distance. You do this also")
+        sleep(6)
+        print("a sailor comes to you, sick with scurvy, a disease from lack of vitamin C, he looks worried")
+        sleep(5)
+        print("Suddenly, you see a ship with strange looking green creatures on board! They have long fangs, hollow eyes, and a long, stroppy mouth,")
+        sleep(5)
+        print("In battles in these times, swords were mainly used on board for defence")
+        sleep(5)
+        while True:
+            choice=input("You have 2 ways of attack; flank or sneak, its all up to you")
+        
+            if choice == "head on ":
+                print("you sneak behind them! the creatures are surprised from your attack! you and 100 men on your ship attack the green creatures!")
+                sleep(5)
+                print("you shoot the cannon, damaging the ship! you leap on the ship, sword in hand, your sailors follow.")
+                sleep(5)
+                print("The green creatures fly they have strange technological devices")
+                sleep(5)
+                print("One of the crew members drops dead right next to you, Another victim of scurvy")
+                sleep(5)
+                print("The crew run at them taking down multiple creatures")
+                sleep(5)
+                print("But it is not enough so you jump overboard to hide")
+                sleep(5)
+                print("Coward! you die")
+            
+    
+    
+            elif choice == "sneak":
+                print("You avoid them and continue on you journey")
+                sleep(2)
+                print("Good choice! continue on and everything will be good!")
+                break
+        
+         
+        print("Did you know that Columbus found San Salvador in October 1492!")
+        sleep(5)
+        print("You make your way there right now!")
+        sleep(5)
+        print("You have to go west to reach San Salvador, but theres a island in your way!")
+        sleep(5)
+        print("Did you know sailor Vasco De Gama sailed around the whole of Africa?")
+        sleep(5)
+        print("In these days islands were inhabited by tribes, so you had to be very careful!")
+        sleep(5)
+        while True:
+            choice=input("You could go Around the island and reach San Salvador, or you try Invade the island and then go to San Salvador, the choice is yours! Type around or invade!")
+        
+            if choice == "around":
+                print("You make your way around, but a hail of arrows attacks your boat!")
+                sleep(5)
+                print("30 out of your 50 men are injured, with 10 dead, but you managed to avoid the bad end of it")
+                sleep(5)
+                print("you make your way around! Alive and well! You use your log and line and latitude and longitude to find your way!")
+                break
+        
+            elif choice == "invade":
+                print("You and your sailors invade the island!")
+                sleep(5)
+                print("you kill most of the men, but its too late as you are stabbed through the chest by a sword from a islander! Unlucky!") #end of Marks code
+            
+        
+        print("Columbus arrived at san salvador in October 1492, but you arrive in 1493!, in a place called Newtown!") #Aarons code
+        sleep(5)
+        print("your a slight bit late, 365 days late!")
+        sleep(5)
+        print("You and your crew set up camp. You can see where columbus has been and you run into the native Indians. these Indians caught diseases very easily such as the common cold, and this could kill them.")
+        sleep(6)
+        print("the indians look worn out, you give them some of your food. Food in these times that were given would be pineapples, corn, maize, etc.")
+        sleep(5)
+        print("you see the fort that Columbus set up, La Navidad, and some of his crew are stationed there! You talk to them, they seem happy to see a familiar face")
+        sleep(5)
+        print("They take you to their hut, and show you what they have. They have lots of gold, food, and maps.")
+        sleep(5)
+        print("they give you some of these materials, and you explore the land. Land these days comtained many unknown species of animals and insects, so explorers would try to find these!")
+        sleep(5)
+        print("You go back to the beach and the soldiers of columbus give you some gold! You board back on your ship and sail away back to Spain! Columbus sailed back to Spain 3 times from his voyages!")
+        sleep(5)
+        print("On Columbuses 3rd voyage home, he was tied up in chains and died a disappointing death! You come back to Spain with glory and riches!")
+        sleep(5)
+        print("You live a happy life and die peacefully in bed!") #end of Aarons code
+        
+        print("You will now have a quiz on the facts about Columbus and the age exploration that was in this code!") #start of Ervins code
+        sleep(5)
+        print("Are you ready? Theres 4 questions.")
+        sleep(5)
+        print("question 1: What year did Columbus start his voyage of exploration?")
+        sleep(5)
+        choice=input("was it A. 1491, B. 1488, C. 1592, or D. 1492. Type in the year.")
+        
+        while True:
+            if choice == "1491":
+                print("Wrong!")
+                
+            elif choice == "1488":
+                print("Wrong!")
+                
+            elif choice == "1592":
+                print("Wrong!")
+                
+            elif choice == "1492":
+                print("Correct!")
+                break
+            
+        print("question 2: Where is christopher colombus born?")
+        sleep(5)
+        choice=input("was it A. Genoa, B. Albufeira, C. Paris, D. Rome. Type in the word.")
+        
+        while True:
+            if choice == "genoa":
+                print("Correct! You are smart.")
+                break
+            elif choice == "albufeira":
+                print("Wrong!")
+                
+            elif choice == "paris":
+                print|("Incorrect!")
+                
+            elif chocie == "rome":
+                print("that answer is not right.") #end of Ervins code
+                
+        
+        print("question 3: Why did Columbus keep his 2 logbooks?") #start of Matthews code
+        sleep(5)
+        choice=input("was it A. because he loved them B. as the crew were worried so he made a fake one with the wrong distance C. he loved writing, and D. he had secrets in them. just type in the letter for this one")
+        
+        while True:
+            if choice == "A":
+                print("not right.")
+                
+            elif choice == "B":
+                print("Correct.")
+                break
+            elif choice == "C":
+                print|("Incorrect!")
+                
+            elif chocie == "D":
+                print("that answer is not right.")
+                
+            
+        
+        
+        print("Question 4: Name 1 of his three ships.")
+        sleep(5)
+        choice=input("was it A. the santa pinta  B. the santa C. the pinta D. the maria pinta. just type in the letter for this one")
+        
+        while True:
+            if choice == "A":
+                print("not right.")
+                
+            elif choice == "B":
+                print("sorry, its wrong.")
+                
+            elif choice == "C":
+                print("Correct")
+                break
+            elif chocie == "D":
+                print("that answer is not right.") #end of Matthews code
+                
+        
+        print("Thanks for playing! Hope you have learned some more history!") #thanks for playing guys!
+                
+            
+        
